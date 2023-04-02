f43f9ee5315abffee75c594e9f8447eecb7e6b3c
kmturhan <kmturhan@gmail.com>
2023-04-02 06:12:03 +0300

    abstract class added

---
+++ Examples/AbstractClasses/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Database database = new SqlServer();
+        database.Add();
+        database.Delete();
+
+
+        Database database2 = new OracleServer();
+        database2.Add();
+        database2.Delete();
+        Console.ReadLine();
+    }
+}
+
+abstract class Database
+{
+    public void Add()
+    {
+        Console.WriteLine("added by default");
+    }
+
+//abstract için "içi dolu olmayan virtual metod diyebiliriz"
+//her class için ayrı ayrı implement etmek gerekiyor
+    public abstract void Delete();
+}
+
+class SqlServer : Database
+{
+    public override void Delete()
+    {
+        Console.WriteLine("deleted by sql");
+    }
+}
+
+class OracleServer : Database
+{
+    public override void Delete()
+    {
+        Console.WriteLine("deleted by oracle");
+    }
+}
