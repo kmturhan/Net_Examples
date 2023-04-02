03c290e5ceefc5eac511657e50fd45690d4765b5
kmturhan <kmturhan@gmail.com>
2023-04-02 06:28:47 +0300

    reacp demo 2 added
    interface example

---
+++ Examples/RecapDemo2/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        Console.ReadLine();
+    }
+}
+
+class CustomerManager
+{
+    public ILogger Logger { get; set; }
+    public void Add()
+    {
+        Logger.Log();
+        Console.WriteLine("Customer added!");
+    }
+}
+
+class Logger: ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to database");
+    }
+}
+
+class FileLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to file");
+    }
+}
+interface ILogger
+{
+    void Log();
+}
