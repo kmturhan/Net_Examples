78d27e3e29034177c3afb411a839611f9063baae
kmturhan <kmturhan@gmail.com>
2023-05-19 23:34:22 +0300

    exception class added

---
+++ Examples/Exceptions/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exceptions;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+try
+{
+	Find();
+}
+catch(RecordNotFoundException exception)
+{
+	Console.WriteLine(exception.Message);
+}
+
+
+static void Find()
+{
+	List<string> student = new List<string>() { "test", "test2", "test3" };
+
+	if (!student.Contains("test6"))
+	{
+		throw new RecordNotFoundException("Record Not Found!");
+	}
+	else
+	{
+		Console.WriteLine("Record Found!");
+	}
+}
+
+
+void catchUse()
+{
+	try
+	{
+		string[] students = new string[] { "test", "test2", "test3" };
+		students[3] = "test4";
+	}
+	catch (IndexOutOfRangeException ex)
+	{
+		Console.WriteLine(ex.Message);
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine(ex.Message);
+	}
+}
+
+
