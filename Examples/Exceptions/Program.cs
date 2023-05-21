ad1d3df48bfa5119a60f74d2c9aba9e12fe2ac6f
kmturhan <kmturhan@gmail.com>
2023-05-22 01:49:35 +0300

    Func added

--- Examples/Exceptions/Program.cs
+++ Examples/Exceptions/Program.cs
@@ -4,11 +4,34 @@ using System.Runtime.CompilerServices;
 
 Console.WriteLine("Hello, World!");
 
+Func<int, int, int> add = Topla;
 
-HandleException(() => {
-	Find();
-});
+Console.WriteLine(add(5,6));
 
+//------------------------
+
+Func<int> getRandomNumber = delegate ()
+{
+	Random random = new Random();
+	return random.Next(1, 100);
+};
+
+Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+Console.WriteLine(getRandomNumber());
+Thread.Sleep(500);
+Console.WriteLine(getRandomNumber2());
+
+
+static int Topla(int x, int y)
+{
+	return x + y;
+}
+static void ActionDemo()
+{
+	HandleException(() => {
+		Find();
+	});
+}
 static void HandleException(Action action)
 {
 	try
