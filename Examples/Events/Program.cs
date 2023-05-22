d3ee973b0f8627c464265fedaf8258798d08be85
kmturhan <kmturhan@gmail.com>
2023-05-22 04:19:48 +0300

    events added

---
+++ Examples/Events/Program.cs
@@ -0,0 +1,20 @@
+﻿using Events;
+
+Product hardDisk = new Product(50);
+hardDisk.ProductName = "Hard Disk";
+
+Product gsm = new Product(50);
+gsm.ProductName = "GSM";
+gsm.StockControlEvent += Gsm_StockControlEvent;
+
+void Gsm_StockControlEvent()
+{
+	Console.WriteLine("Gsm about to finish!");
+}
+
+for (int i = 0; i < 10; i++)
+{
+	hardDisk.Sell(10);
+	gsm.Sell(10);
+	Console.ReadLine();
+}
