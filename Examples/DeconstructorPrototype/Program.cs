e54531ad0093be0d2cffb269501e892df919fde3
kmturhan <kmturhan@gmail.com>
2023-08-20 16:04:24 +0300

    deconstructer added

---
+++ Examples/DeconstructorPrototype/Program.cs
@@ -0,0 +1,47 @@
+﻿
+List<Person> list = new List<Person>();
+list.Add(new Person
+{
+    Name = "Test1",
+    Age = 10,
+    City = "İstanbul"
+});
+
+list.Add(new Person
+{
+    Name = "Test2",
+    Age = 20,
+    City = "Kocaeli"
+});
+
+list.Add(new Person
+{
+    Name = "Test3",
+    Age = 30,
+    City = "Sakarya"
+});
+
+
+foreach (var item in list)
+{
+
+    var (itemName, itemAge) = item;
+    Console.WriteLine($"{itemName} - {itemAge}");        
+}
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string City{ get; set; }
+
+    /// <summary>
+    /// Deconstruct Tuple tipinde sadece belirlediğin prop'ları dönmeni sağlayan bir araç.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="age"></param>
+    public void Deconstruct(out string name, out int age)
+    {
+        name = Name;
+        age = Age;
+    }
+}
