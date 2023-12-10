81b689ce80f5ed9432d10de4e5402eb9eafc7898
kmturhan <kmturhan@gmail.com>
2023-12-11 00:58:21 +0300

    base and this keywords

---
+++ Examples/BaseAndThisKeywords/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+
+
+public class BaseClass
+{
+    public BaseClass(int a)
+    {
+
+    }
+    public BaseClass(string name, int c)
+    {
+        Console.WriteLine($"{name} : {c}");
+    }
+    public BaseClass(string name, int c, int d)
+    {
+        Console.WriteLine($"{name} : {c}");
+    }
+}
+
+public class DerivedClass : BaseClass
+{
+    public DerivedClass() : base(5)
+    {
+
+    }
+    public DerivedClass(string a) : base(a, 2)
+    {
+
+    }
+}
